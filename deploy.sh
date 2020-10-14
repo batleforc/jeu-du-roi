ffa154a9070496b9a28740ebc151285a12e5e85f
Maxime <maxleriche.60@gmail.com>
2020-10-15 01:27:37 +0200

    add tags + indent correct

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git_message=$(git log --oneline -n 1)
 dock_message=$(docker logs akau)
-messagefinal='"GIT: '$git_message"\nDocker: "$dock_message"\nCI_URL:"$PROJECT_URL"\nUserWhoStart:"$USER_START'"'
+messagefinal='GIT: '$git_message"\nDocker: "$dock_message"\nCI_URL:"$PROJECT_URL"\nUserWhoStart:"$USER_START
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/735175356054765618/MdOM3hDJPPEi1HBnCDJWhPQNK78-dOShV_mbu2dR4PfQfUZd_m0T3AMff670UMgqKCr4"
 echo $messagefinal
 curl -X POST \
@@ -11,7 +11,7 @@ curl -X POST \
             \"title\":\"Update du site de linterface du projet\",
             \"color\":\"707070\",
             \"url\":\"https://akau.weebo.fr/\",
-            \"description\":$messagefinal
+            \"description\":\"$messagefinal\"
         }
     ]}" \
     $WEBHOOK_URL
