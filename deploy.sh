1210562dad543a2462fcae8ceae0b240e6e0b224
Maxime <maxleriche.60@gmail.com>
2020-10-15 11:36:40 +0200

    **insert une musique déprimante**

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 git_message=$(git log --oneline -n 1)
 dock_message=$(docker logs akau)
-messagefinal="\"GIT: "$git_message"\nDocker: "$dock_message"\nCI_URL: "$PROJECT_URL"\nUserWhoStart:"$USER_START"\""
+messagefinal="\"GIT: "$git_message" \nDocker: "$dock_message" \nCI_URL: "$PROJECT_URL" \nUserWhoStart:"$USER_START" \""
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/735175356054765618/MdOM3hDJPPEi1HBnCDJWhPQNK78-dOShV_mbu2dR4PfQfUZd_m0T3AMff670UMgqKCr4"
 echo $PROJECT_URL
 echo $USER_START
 echo $messagefinal
-curl -X POST \
+curl -i -X POST \
     -H "Content-Type: application/json" \
     -d "{
         \"embeds\":[
