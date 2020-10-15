9b711d73b6e8f0e293ed27f89a379af298209c79
Maxime <maxleriche.60@gmail.com>
2020-10-15 09:30:04 +0200

    test changement curl message

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@ git_message=$(git log --oneline -n 1)
 dock_message=$(docker logs akau)
 messagefinal='GIT: '$git_message"\nDocker: "$dock_message"\nCI_URL:"$PROJECT_URL"\nUserWhoStart:"$USER_START
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/735175356054765618/MdOM3hDJPPEi1HBnCDJWhPQNK78-dOShV_mbu2dR4PfQfUZd_m0T3AMff670UMgqKCr4"
-echo $messagefinal
-request="{
+
+echo $request
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d "{
         \"embeds\":[
         {
             \"title\":\"Update du site de linterface du projet\",
@@ -11,9 +14,5 @@ request="{
             \"url\":\"https://akau.weebo.fr/\",
             \"description\":\"$messagefinal\"
         }
-    ]}"
-echo $request
-curl -X POST \
-    -H "Content-Type: application/json" \
-    -d $request \
+    ]}" \
     $WEBHOOK_URL
