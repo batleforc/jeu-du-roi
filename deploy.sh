31fdced5988690d865d0a172f70c02969899587f
Maxime <maxleriche.60@gmail.com>
2020-10-15 08:28:45 +0200

    test echo request

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,7 @@ dock_message=$(docker logs akau)
 messagefinal='GIT: '$git_message"\nDocker: "$dock_message"\nCI_URL:"$PROJECT_URL"\nUserWhoStart:"$USER_START
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/735175356054765618/MdOM3hDJPPEi1HBnCDJWhPQNK78-dOShV_mbu2dR4PfQfUZd_m0T3AMff670UMgqKCr4"
 echo $messagefinal
-curl -X POST \
-    -H "Content-Type: application/json" \
-    -d "{
+request="{
         \"embeds\":[
         {
             \"title\":\"Update du site de linterface du projet\",
@@ -13,5 +11,9 @@ curl -X POST \
             \"url\":\"https://akau.weebo.fr/\",
             \"description\":\"$messagefinal\"
         }
-    ]}" \
+    ]}"
+echo $request
+curl -X POST \
+    -H "Content-Type: application/json" \
+    -d $request \
     $WEBHOOK_URL
