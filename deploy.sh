68daffc1357f76c237c7ce5637d95b1df2c6f46c
Maxime <maxleriche.60@gmail.com>
2020-10-15 09:55:05 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 git_message=$(git log --oneline -n 1)
 dock_message=$(docker logs akau)
-messagefinal="\"GIT: "$git_message"\nDocker: "$dock_message"\nCI_URL: "$PROJECT_URL"\nUserWhoStart: "$USER_START"\""
+messagefinal="\"GIT: \""$git_message"\"\nDocker: \""$dock_message"\"\nCI_URL: \""$PROJECT_URL"\"\nUserWhoStart: \""$USER_START"\"\""
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/735175356054765618/MdOM3hDJPPEi1HBnCDJWhPQNK78-dOShV_mbu2dR4PfQfUZd_m0T3AMff670UMgqKCr4"
 request="\"{\"embeds\":[{\"title\":\"Update du site de linterface du projet\",\"color\":\"707070\",\"url\":\"https://akau.weebo.fr/\",\"description\":"$messagefinal"}]}\""
 echo $request
