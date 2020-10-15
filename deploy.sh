48ece9c428ef1da6767ed048b9baec187191a990
Maxime <maxleriche.60@gmail.com>
2020-10-15 12:00:01 +0200

    Test qui ne fonctionnera pas

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 git_message=$(git log --oneline -n 1)
 dock_message=$(docker logs akau)
 messagefinal="\"GIT: "$git_message" \nDocker: "$dock_message" \nCI_URL: "$PROJECT_URL" \nUserWhoStart: "$USER_START" \""
+messagefinal="GIT: 115ca68 HEAVY METAL \nDocker: app is listening on port 8080 \nCI_URL: https://gitlab.com/a-king-among-us/jeu-du-roi-ui/-/pipelines/202937679 \nUserWhoStart: maxLeriche.weebo "
 export WEBHOOK_URL="https://discordapp.com/api/webhooks/735175356054765618/MdOM3hDJPPEi1HBnCDJWhPQNK78-dOShV_mbu2dR4PfQfUZd_m0T3AMff670UMgqKCr4"
 echo $PROJECT_URL
 echo $USER_START
 echo $messagefinal
-curl -i -X POST \
+curl -X POST \
     -H "Content-Type: application/json" \
     -d "{
         \"embeds\":[
